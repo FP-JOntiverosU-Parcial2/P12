ea7e4801a067581fec454d89bce9502ee97f904e
jaime <jaime@vaio>
2016-10-20 23:51:11 -0500

    problema 12

--- src/problema12/Problema12.java
+++ src/problema12/Problema12.java
@@ -15,31 +15,31 @@ public class Problema12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         int [] cad = new int[10];
-        cad=llenarCad(cad);
-        mostrarCad(cad);
+         int [] arr = new int[10];
+        arr=llenarArr(arr);     //se llena el arreglo con numeros
+        mostrarArr(arr);  //se hace la operacion deseada
         mensajeSalida();
         
     }
-    public static int[] llenarCad(int[] cad){
+    public static int[] llenarArr(int[] arr){
         Scanner entrada = new Scanner(System.in);
-        for(int i=0;i<cad.length;i++){
-            System.out.println("Introduce un numero para la posicion "+ (i+1) +" :");
-            cad[i]=entrada.nextInt();
+        for(int i=0;i<arr.length;i++){
+            System.out.print("Introduce un numero para la posicion "+ (i+1) +" :");
+            arr[i]=entrada.nextInt();
         }
-    return cad;
+    return arr;
     }
     
-    public static void mostrarCad(int[] cad){
-        int total=0,pares=0;
-        for (int i=1; i<cad.length; i+=2){
-            total=cad[i]+total;
+    public static void mostrarArr(int[] arr){         
+        int total=0,pares=0;                       //se hace una operacion donde los pares se suman
+        for (int i=1; i<arr.length; i+=2){ //y mientas se tenga un par se agregara a "Pares" para dividirlo 
+            total=arr[i]+total;
             pares++;
         }
         System.out.println("La media de los numeros en las posiciones pares es: " + total/pares);
     }
     public static void mensajeSalida(){
-        System.out.println("Gracias por usar el programa"); 
+        System.out.println("Gracias por usar el programa");  //se agradece por usar el programa
         System.exit(0);
     }
 }
